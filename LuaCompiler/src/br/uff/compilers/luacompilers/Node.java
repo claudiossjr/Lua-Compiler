2b4f68e3a24bca1ae1e67e1f1c61dcdcbdeb1510
Claudio Santos <c.souza0804@gmail.com>
2016-12-20 23:57:07 -0200

    Subindo

---
+++ LuaCompiler/src/br/uff/compilers/luacompilers/Node.java
@@ -0,0 +1,55 @@
+package br.uff.compilers.luacompilers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    private Token info;
+    private List<Node> children;
+    
+    public Node(){
+        this(null);
+    }
+    
+    public Node(Token info) {
+        this.info = info;
+        this.children = new ArrayList<>();
+    }  
+    
+    public void addChildren(Token info){
+        children.add(new Node(info));
+    }
+    
+    public void addChildren(Node node){
+        children.add(node);
+    }
+    
+    @Override
+    public String toString() {
+        print();
+        return "";
+    }
+
+    public void print() {
+        print("", true);
+    }
+
+    private void print(String prefix, boolean isTail) {
+        String tab = "";
+        String space = "";
+        if(isTail){
+            tab = "└── ";
+            space ="    ";
+        } else{
+            tab = "├── ";
+            space = "│   ";
+        }    
+        System.out.println(prefix + tab + info.val);
+        for (int i = 0; i < children.size() - 1; i++) {
+            children.get(i).print(prefix + space, false);
+        }
+        if (children.size() > 0) {
+            children.get(children.size() - 1).print(prefix + space, true);
+        }
+    }
+}
